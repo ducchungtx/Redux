f8f6958449131ab96e06d818626ee47d82354d37
Chung Nguyen <zrmedia9@gmail.com>
2017-07-05 20:39:48 +0700

    Redux Basic (NodeJS SV)

---
+++ index.js
@@ -0,0 +1,21 @@
+const { createStore } = require('redux');
+
+const defaultState = { value: 0 };
+
+const reducer = (state = defaultState, action) => {
+    if(action.type === 'UP') return { value: state.value + 1 };
+    if(action.type === 'DOWN') return { value: state.value - 1 };
+    return state;
+};
+
+const store = createStore(reducer);
+console.log('TRUOC KHI DISPATCH: ' + store.getState().value);
+store.dispatch({ type: 'UP' });
+store.dispatch({ type: 'UP' });
+store.dispatch({ type: 'UP' });
+store.dispatch({ type: 'DOWN' });
+console.log('TRUOC KHI DISPATCH: ' + store.getState().value);
+
+// Action: UP, DOWN
+// Lam sao tiep can gia tri cua state
+// Lam sao thay doi gia tri state
